e23910ee5b3c7e7f344a7ab642f0fa8305fa6739
Michael Zürcher <michzuerch@gmail.com>
2022-03-21 12:16:21 +0100

    Carousel

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-		prerender: {
+		
+    vite: { optimizeDeps: { include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep'] } },
+    prerender: {
 			// This can be false if you're using a fallback (i.e. SPA mode)
 			default: true
 		}
@@ -17,3 +19,4 @@ const config = {
 };
 
 export default config;
+
