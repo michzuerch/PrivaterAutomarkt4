9867fd16c507a1ec868c3287245c2d28a3a8718d
Michael Zuercher <michzuerch@gmail.com>
2022-09-29 15:50:42 +0200

    build without errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,3 @@
-<script>
-	import LeafletMap from '$lib/LeafletMap.svelte';
-</script>
-
-<main>
-	<LeafletMap />
-</main>
 
 <h1 class="text-3xl font-bold underline">
   Hello world!
--- svelte.config.js
+++ svelte.config.js
@@ -10,8 +10,15 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+		  // default options are shown. On some platforms
+		  // these options are set automatically — see below
+		  pages: 'build',
+		  assets: 'build',
+		  fallback: null,
+		  precompress: false
+		})
+	  }
 };
 
 export default config;
