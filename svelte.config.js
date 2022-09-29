0c37fe39d16dbfbb5dc207044cb718989f7656cd
Michael Zuercher <michzuerch@gmail.com>
2022-09-29 14:06:10 +0200

    Tailwind and static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 	kit: {
 		adapter: adapter()
 	}
