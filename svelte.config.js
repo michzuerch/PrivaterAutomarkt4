a75b2cc4b3880516278239ca68eeb9bacd4a59e0
Michael Zuercher <michzuerch@gmail.com>
2022-10-01 11:46:46 +0200

    eslint, prettier and tests working, node-adapter as fallback

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,18 @@
-//import adapter from '@sveltejs/adapter-auto';
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
+import adapterNode from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+export default {
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter({
-		  // default options are shown. On some platforms
-		  // these options are set automatically — see below
-		  pages: 'build',
-		  assets: 'build',
-		  fallback: null,
-		  precompress: false
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: adapterNode,
+			precompress: false
 		})
-	  }
+	}
 };
-
-export default config;
