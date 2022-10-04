1f0ef0766c84b7982be64bd4d2ef5f10677a9154
Michael Zuercher <michzuerch@gmail.com>
2022-10-04 11:41:33 +0200

    format

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -29,11 +29,15 @@
 </div>
 
 <div class="collapse">
-	<input type="checkbox" class="peer" /> 
-	<div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-	  Click me to show/hide content
+	<input type="checkbox" class="peer" />
+	<div
+		class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"
+	>
+		Click me to show/hide content
 	</div>
-	<div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"> 
-	  <p>hello</p>
+	<div
+		class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"
+	>
+		<p>hello</p>
 	</div>
-  </div>
+</div>
